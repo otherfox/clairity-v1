e50674e4589cc1e6a973d0482910af8a08cfa39b
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-18 21:59:27 -0400

    Login working; logout in progress

--- node_modules/clairity-client/src/login/services.js
+++ node_modules/clairity-client/src/login/services.js
@@ -3,21 +3,23 @@ import _ from 'lodash'
 
 import req from 'superagent'
 
-export function tryUserLogin(username, password) {
-  // new Promise((resolve, reject) => {
-  //   req.post("TODO: SCOTT HALP")
-  //     .withCredentials()
-  //     .send({username, password}) // ES6 has the same cool feature as coffeescript
-  //     .end((err, res) => {
-  //       console.log(err);
-  //       console.log(res);
-  //     });
-  // });
-  return new Promise((res, rej) =>
-    _.defer(() =>
-      Math.random() > 0.5 ?
-        res(data.user) :
-        rej(data.reason)
-    )
-  );
+export function tryUserLogin(creds) {
+  return new Promise((resolve, reject) => {
+    req.post("http://lab.rairity.com/login.cfm")
+      .withCredentials()
+      .type('form')
+      .send(creds)
+      .end((err, res) => {
+        console.log(err);
+        console.log(res);
+        debugger;
+      });
+  });
+  // return new Promise((res, rej) =>
+  //   _.defer(() =>
+  //     Math.random() > 0.5 ?
+  //       res(data.user) :
+  //       rej(data.reason)
+  //   )
+  // );
 }
