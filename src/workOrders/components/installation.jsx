f01b402c52bf01316da4f907502a1e91a9683dfc
Nick Tennies <nick@brainbytescreative.com>
2015-06-19 16:11:15 -0400

    adds work order component files

---
+++ src/workOrders/components/installation.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Settings from '../shared/components/settings'
+
+let data = [];
+
+let Installation = React.createClass ({
+
+  propTypes: {
+    style: React.PropTypes.object,
+    data: React.PropTypes.array
+  },
+
+  getDefaultProps() {
+    return {
+      data: data
+    };
+  },
+  style() {
+    let style = {};
+
+    if(this.props.style) {
+      Object.keys(this.props.style).forEach(function(key, i){
+        console.log(key);
+        style[key] = this.props.style[key];
+      }, this);
+    }
+
+    return style;
+  },
+
+  render() {
+    return (
+      <div style={this.style()}>
+        <h3>Installation</h3>
+      </div>
+    );
+  }
+});
+
+export default Installation;
