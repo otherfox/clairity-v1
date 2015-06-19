44b499c6a7031c2477e657caf2bd3ece44dffd32
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-19 15:01:20 -0400

    Added query for work order messages

---
+++ src/workOrders/services/workOrder.js
@@ -0,0 +1,39 @@
+// import data from './stubs/orders'
+import _ from 'lodash'
+import moment from 'moment'
+
+import req from 'superagent'
+
+export function getWorkOrder(id) {
+  return new Promise((s, f) => {
+    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.WorkOrderDAO&_m=getWorkOrderById&id=${id}`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
+
+export function getWorkOrderMessages(id) {
+  return new Promise((s, f) => {
+    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.WorkOrderMessageDAO&_m=getAllWorkOrderMessagesByWorkOrderId&work_order_id=${id}`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
+
+export function putWorkOrder(id, data) {
+    return new Promise((s, f) => {
+
+    });
+}
