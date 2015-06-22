303f92403028efcb8dfafeb4e9c2d52ada01e40d
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-22 17:10:04 -0400

    Removed debug statement

--- src/shared/components/networkRenderer.jsx
+++ src/shared/components/networkRenderer.jsx
@@ -56,7 +56,6 @@ export default function(Component, tableName) {
 
     render() {
       if (this.state.ready) {
-        debugger;
         return <Component {...this.props} {...{[tableName]: this.state.data}} />
       } else {
         return false;
