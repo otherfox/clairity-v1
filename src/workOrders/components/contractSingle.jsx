2497f44147c4cb1d4eb46566771efe0fcc82d3b3
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-23 01:16:31 -0400

    Fixed typo prevent agent name from rendering

--- src/workOrders/components/contractSingle.jsx
+++ src/workOrders/components/contractSingle.jsx
@@ -37,7 +37,7 @@ const ContractSingle = React.createClass({
     return [
       {
         label: 'Agent',
-        value: contract.get['agent_name']
+        value: contract.get('agent_name')
       },
       {
         label: 'Type',
