4e3c904a69ff69d9fdb8b23aa3d4e0c5d22ab568
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-23 20:13:13 -0400

    Added networkCollectionRenderer

---
+++ src/shared/components/networkRenderer/collection.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import delayRender from './base'
+import Store, {MessageTypes} from '../store'
+import {fromJS} from 'immutable'
+
+export function networkCollectionRenderer(Component, options) {
+
+  let Delayed = delayRender(Component, {
+    writeMethod: data => Store.handleMessage({
+      type: options.replace ? MessageTypes.ReplaceAll : MessageTypes.Write,
+      payload: {
+        rows: data,
+        table: options.tableName
+      }
+    }),
+    shouldFetch: e => e.state.data,
+    cacheMethod: () => Store.data.get(options.tableName).toList(),
+    serviceMethod: options.serviceMethod,
+    propName: options.propName || options.tableName + 's'
+  });
+
+  return class networkCollectionRenderer extends React.Component {
+    render() {
+      return <Wrapped id={this.props.id} options={opts} />
+    }
+  }
+}
