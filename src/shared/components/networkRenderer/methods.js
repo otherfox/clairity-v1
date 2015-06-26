037658e14cbcf07f6e0fbd88be5ffa3a09efc629
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-26 17:42:31 -0400

    I don't know how JavaScript arrays work

--- src/shared/components/networkRenderer/methods.js
+++ src/shared/components/networkRenderer/methods.js
@@ -1,7 +1,8 @@
 
 export function exposeMethods(methods) {
   return (target) => {
-    for (let method in methods) {
+    for (let n in methods) {
+      let method = methods[n];
       target.prototype[method] = function() {
         let ref = this.refs.inner;
         return ref[method].apply(ref, arguments);
