4a9d0027a2c21eb3809bfd9338b22918650c50a3
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-26 17:22:32 -0400

    Added an 'exposeMethods' decorator for the network renderers

---
+++ src/shared/components/networkRenderer/methods.js
@@ -0,0 +1,11 @@
+
+export default function exposeMethods(methods) {
+  return (target) => {
+    let ref = this.refs.inner;
+    for (let method in methods) {
+      target.prototype[method] = function() {
+        return ref[method].apply(ref, arguments);
+      }
+    }
+  }
+}
