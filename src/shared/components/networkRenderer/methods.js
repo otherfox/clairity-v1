a810e321fbfabfb76fa63a1b39bfa024adac76d5
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-26 17:28:37 -0400

    Fixed property access in decorator method

--- src/shared/components/networkRenderer/methods.js
+++ src/shared/components/networkRenderer/methods.js
@@ -1,9 +1,9 @@
 
 export function exposeMethods(methods) {
   return (target) => {
-    let ref = this.refs.inner;
     for (let method in methods) {
       target.prototype[method] = function() {
+        let ref = this.refs.inner;
         return ref[method].apply(ref, arguments);
       }
     }
