bd5895c93d86754d4c72657a2bc9d90015acaca5
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-06-29 14:37:42 -0400

    Fixed broken Find+Replace in work order actions

--- src/shared/actions/workOrder.js
+++ src/shared/actions/workOrder.js
@@ -26,5 +26,5 @@ workOrderFetched.listen(workOrder =>
 export const updateWorkOrder = Fynx.createAction();
 
 updateWorkOrder.listen((state) => {
-  return updateWorkOrder(state.workOrder, state.sameAddress);
+  return putWorkOrder(state.workOrder, state.sameAddress);
 });
