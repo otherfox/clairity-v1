1972291a1f3745aeea3cf8414774b4a450b33620
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-01 04:29:46 -0400

    Started in on an owners component

---
+++ src/workOrders/components/details/owner.js
@@ -0,0 +1,28 @@
+import React, {addons} from 'react/addons'
+import DropDown from '../../../shared/components/dropDown'
+import delayRender from '../../../shared/components/base'
+import {ownersFetched} from '../../actions'
+import {queryOwners} from '../../queries'
+import {getOwners} from '../../services/users'
+
+let WorkOrderDetailsOwner = React.createClass({
+  mixins: [addons.LinkedStateMixin],
+  getInitialState() {
+    return {
+      owner: this.props.workOrder.owner_id || this.props.owners.first().get('id')
+    };
+  },
+  render() {
+    return (
+      <DropDown menuItems={this.props.owners} valueLink={this.linkState('owner')} />
+    );
+  }
+});
+
+export default delayRender(WorkOrderDetailsOwner, {
+  writeMethod: ownersFetched,
+  shouldFetch: e => e.state.data,
+  cacheMethod: queryOwners,
+  serviceMethod: getOwners,
+  propName: 'owners'
+});
