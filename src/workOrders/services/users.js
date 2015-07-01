fc9428d2a16c31a88210ec862111f79b8d232fb8
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-01 04:29:17 -0400

    Fixed getPermissionByRole and added a getOwners

--- src/workOrders/services/users.js
+++ src/workOrders/services/users.js
@@ -3,9 +3,23 @@ import moment from 'moment'
 
 import req from 'superagent'
 
+export function getUser(id) {
+  return new Promise((s, f) => {
+    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.UserDAO&_m=getUserById&id=${id}`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
+
 export function getPermissionByRole(role) {
   return new Promise((s, f) => {
-    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.ContactDAO&_m=getContactById&id=${id}`)
+    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.UserDAO&_m=getPermissionByRoleName&roleName=${id}`)
       .withCredentials()
       .end((err, res) => {
         if (!err) {
@@ -29,4 +43,9 @@ export function getUsersByRole(role, active = true) {
         }
       });
   });
+}
+
+export function getOwners() {
+  return Promise.all(getUsersByRole('provisioning'), getUsersByRole('field_ops'))
+    .then(results => _.uniq(results[0].concat[results[1]], 'id'));
 }
