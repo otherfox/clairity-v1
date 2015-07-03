855f3447ca0ec1bd4ad70f89a947d96c26bdbeda
Nicholas Tennies <nick@brainbytescreative.com>
2015-07-03 15:07:45 -0400

    adds compact view switch to settings

--- src/settings/index.jsx
+++ src/settings/index.jsx
@@ -1,6 +1,7 @@
 
 import React, {PropTypes} from 'react'
 import Layout from  '../shared/components/layout'
+import Details from  '../shared/components/details'
 import Footer from  '../shared/components/footer'
 import TopNav from '../shared/components/topnav'
 import LeftNav from '../shared/components/leftnav'
@@ -25,16 +26,26 @@ import controllable from 'react-controllables'
 
 import {Navigation} from 'react-router'
 
-let OpenInstalls = React.createClass({
+let Settings = React.createClass({
   mixins: [Navigation],
 
   render() {
     return (
-      <div>
-        Settings Page
-      </div>
+      <Layout widths={{}} cPadding={'20px 20px 0 0'}>
+      <Paper>
+        <Layout widths={{}} cPadding={'0 20px 20px 20px'}>
+          <Details
+            title={'Settings'}
+            data={[
+                {label: '', value: <Layout><Toggle labelStyle={{ minWidth: '100px'}} name="compactView"  value="false"  label="Compact View" /></Layout>},
+                {},
+            ]}
+          />
+        </Layout>
+      </Paper>
+    </Layout>
     );
   }
 });
 
-export default OpenInstalls;
+export default Settings;
