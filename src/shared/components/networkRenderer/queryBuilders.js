5ef13cfd231577191c6d1a6d81048c7dbc51feae
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-07 12:13:10 -0400

    Added generic collection queries and simplified queryBuilders

--- src/shared/components/networkRenderer/queryBuilders.js
+++ src/shared/components/networkRenderer/queryBuilders.js
@@ -4,12 +4,12 @@ import Store, {MessageTypes} from '../../store'
 import {getResource} from '../../services/getResource'
 import {fromJS} from 'immutable'
 
-export function modelQuery(tableName, options) {
-  let id = options ? options.id || `${tableName}Id` : `${tableName}Id`;
+export function modelQuery(tableName, propName, methods = []) {
+  let id = `${tableName}Id`;
   return {
     tableName,
-    propName: tableName,
-    methods: options ? options.methods || [] : [],
+    methods,
+    propName: propName || tableName,
     cacheMethod: props => Store.data.getIn([tableName, props[id]]),
     serviceMethod: props => getResource(props[id], tableName),
     writeMethod: data => Store.handleMessage({
@@ -22,21 +22,21 @@ export function modelQuery(tableName, options) {
   }
 }
 
-export function collectionQuery(options) {
+export function collectionQuery(tableName, propName, methods = []) {
   return {
-    tableName: options.tableName,
-    methods: options ? options.methods || [] : [],
+    tableName,
+    methods,
     propName: options.propName || options.tableName + 's',
-    serviceMethod: options.serviceMethod,
+    serviceMethod: props => getCollection(tableName),
     cacheMethod: () => {
-      let results = Store.data.get(options.tableName).toList();
+      let results = Store.data.get(tableName).toList();
       return results.size > 0 ? results : null;
     },
     writeMethod: data => Store.handleMessage({
       type: options.replace ? MessageTypes.ReplaceAll : MessageTypes.Write,
       payload: {
         rows: data,
-        table: options.tableName
+        table: tableName
       }
     })
   }
