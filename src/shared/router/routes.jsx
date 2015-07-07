87dd06a9dc38ae57d4601413adc2b7e4deb559a2
Nick Tennies <nick@brainbytescreative.com>
2015-07-07 10:49:36 -0400

    adds ip blocks, ip zones, view lead and edit lead to routes

--- src/shared/router/routes.jsx
+++ src/shared/router/routes.jsx
@@ -6,12 +6,16 @@ import AgingReports from '../../agingReports'
 import CreateContract from '../../createContract'
 import CreateLead from '../../createLead'
 import EditCustomer  from '../../editCustomer'
+import EditLead from '../../editLead'
+import IpBlocks from '../../ipBlocks'
+import IpZones from '../../ipZones'
 import Login from '../../login'
 import OpenInstalls from '../../openInstalls'
 import Settings from '../../settings'
 import WorkOrders from '../../workOrders'
 import WorkOrderUpload from '../../workOrderUpload'
 import ViewCustomer from '../../viewCustomer'
+import ViewLead from '../../viewLead'
 import ViewLeads from '../../viewLeads'
 
 
@@ -26,7 +30,12 @@ export default (
         <Route name="edit-customer" path="edit" handler={EditCustomer} />
       </Route>
       <Route name="create-contract" handler={CreateContract} />
-      <Route name="create-lead/:id/:agentid" handler={CreateLead} />
+      <Route name="ip-blocks" handler={IpBlocks} />
+      <Route name="ip-zones" handler={IpZones} />
+      <Route name="lead/:id/:agentid" handler={RouteHandler} />
+        <DefaultRoute name="view-lead" handler={ViewLead} />
+        <Route name="edit-lead" path="edit" handler={EditLead} />
+        <Route name="create-lead" path="create" handler={CreateLead} />
       <Route name="open-installs" handler={OpenInstalls} />
       <Route name="work-orders" path="work-orders/:id" handler={WorkOrders}>
         <Route name="work-order-upload" path="upload" handler={WorkOrderUpload} />
