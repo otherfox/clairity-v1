784b7db03209746490261c23bd30b90837794fe3
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-09 19:37:53 -0400

    services/contacts.getLeads()

--- src/shared/services/contacts.js
+++ src/shared/services/contacts.js
@@ -4,6 +4,8 @@
 import _ from 'lodash'
 import moment from 'moment'
 
+import {debug} from '../mixins/debug'
+
 import req from 'superagent'
 
 export function getContact(id) {
@@ -19,3 +21,19 @@ export function getContact(id) {
       });
   });
 }
+
+export function getContactsWithoutOpportunities() {
+  return new Promise(function(resolve, reject) {
+    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.ContactDAO&_m=getAllContactsWithoutOpportunities`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
+
+debug('getLeads')(getContactsWithoutOpportunities);
