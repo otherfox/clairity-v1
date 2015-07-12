ec2fa1abeca64fb946152296185b03096de4d4d0
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-12 00:26:11 -0400

    Model now uses queryRenderer instead of DelayRenderer

--- src/shared/components/networkRenderer/model.jsx
+++ src/shared/components/networkRenderer/model.jsx
@@ -5,31 +5,15 @@ import {getResource} from '../../services/getResource'
 import {fromJS} from 'immutable'
 import {exposeMethods} from './methods'
 
-export function networkModelRenderer(Component, tableName, options) {
+import {modelQuery} from './queryBuilders'
+import multiQueryRenderer from './queryRenderer'
 
-  let Delayed = delayRender(Component, {
-    tableName,
-    cacheMethod: props => Store.data.getIn([tableName, props.id]),
-    serviceMethod: props => getResource(props.id, tableName),
-    propName: tableName,
-    shouldFetch: (e, p) => e.state.data && e.state.data.get('id') != p.id,
-    methods: options ? options.methods : [],
-    writeMethod: data => Store.handleMessage({
-      type: MessageTypes.Write,
-      payload: {
-        table: tableName,
-        row: data
-      }
-    })
-  })
+export function networkModelRenderer(Component, tableName, options = {}) {
 
-  @exposeMethods(options ? options.methods || [] : [])
-  class NetworkModelWrapper extends React.Component {
-    render() {
-      return <Delayed {...this.props} />
-    }
-  }
-
-  return NetworkModelWrapper;
-
+  return multiQueryRenderer(Component, {
+    methods: options.methods || [],
+    queries: [
+      modelQuery(tableName, tableName, 'id')
+    ]
+  });
 }
