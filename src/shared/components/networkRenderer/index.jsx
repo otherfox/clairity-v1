1bc301a75e7c1e686cc5c349e6abcec8010e7273
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-12 05:48:47 -0400

    Fixed queryRenderer re-export

--- src/shared/components/networkRenderer/index.jsx
+++ src/shared/components/networkRenderer/index.jsx
@@ -1,5 +1,5 @@
 export {networkModelRenderer} from './model'
 export {networkCollectionRenderer} from './collection'
 
-export {queryRenderer} from './queryRenderer'
+export queryRenderer from './queryRenderer'
 export {modelQuery, collectionQuery} from './queryBuilders'
