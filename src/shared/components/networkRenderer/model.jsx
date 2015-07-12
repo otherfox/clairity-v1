948e8ece19083c795d9bf853c6667f392685ac9c
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-12 03:43:07 -0400

    Formatting

--- src/shared/components/networkRenderer/model.jsx
+++ src/shared/components/networkRenderer/model.jsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import delayRender from './base'
-import Store, {MessageTypes} from '../../store'
-import {getResource} from '../../services/getResource'
-import {fromJS} from 'immutable'
-import {exposeMethods} from './methods'
 
 import {modelQuery} from './queryBuilders'
 import multiQueryRenderer from './queryRenderer'
