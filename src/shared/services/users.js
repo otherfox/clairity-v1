c282151d58214fe1c75ce6416995c1706293a4ba
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-12 13:13:03 -0400

    Adds a getUsersByType service method

--- src/shared/services/users.js
+++ src/shared/services/users.js
@@ -45,6 +45,20 @@ export function getUsersByRole(role, active = true) {
   });
 }
 
+export function getUsersByType(type, active = true) {
+  return new Promise((s, f) => {
+    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.UserDAO&_m=getAllUsersByType&type=${type}&active=${active}`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
+
 export function getWorkOrderOwners() {
   return Promise.all([getUsersByRole('provisioning'), getUsersByRole('field_ops')])
     .then(results => _.uniq(results[0].concat(results[1]), 'id'));
