292ee2fe2755113c16659075dab2f89290e051c7
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-13 15:06:56 -0400

    Adds salesStage dropdown

---
+++ src/createLead/salesStage.jsx
@@ -0,0 +1,28 @@
+import React, {PropTypes} from 'react'
+import DropDown from '../shared/components/dropDown'
+import {Map} from 'immutable'
+import {
+  networkCollectionRenderer
+} from '../shared/components/networkRenderer'
+
+class ProjectTypesView extends React.Component {
+  getMenuItems() {
+    return this.props.salesStages.map(o => new Map({
+      label: o.get('name'),
+      value: o.get('id')
+    })).unshift(new Map({label: "", value: ""}));
+  }
+  render() {
+    return <DropDown {...this.props}
+                     menuItems={this.getMenuItems()} />
+  }
+}
+ProjectTypesView.displayName = 'ProjectTypesInternal';
+
+let ProjectTypes = networkCollectionRenderer(ProjectTypesView, {
+  tableName: 'salesStage',
+});
+
+ProjectTypes.displayName = 'ProjectTypes';
+
+export default ProjectTypes;
