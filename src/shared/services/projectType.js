a66f1a56d2c3a43b4691ea85bf1d9788e43e281a
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-13 14:44:44 -0400

    Adds salesProjectType get request

---
+++ src/shared/services/projectType.js
@@ -0,0 +1,20 @@
+//SalesProjectTypeDAO
+
+import _ from 'lodash'
+import moment from 'moment'
+
+import req from 'superagent'
+
+export function getProjectTypes() {
+  return new Promise((s, f) => {
+    req.get(`http://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.SalesProjectTypeDAO&_m=getAllSalesProjectTypes`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
