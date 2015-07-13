0968e7c8570248f76075a6b398b45dd0372391ab
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-13 14:37:14 -0400

    create lead owners dropdown getMenuItems

--- src/createLead/owners.jsx
+++ src/createLead/owners.jsx
@@ -2,7 +2,8 @@ import React, {PropTypes} from 'react'
 import {usersFetched} from '../shared/actions/user'
 import {queryAccountOwners} from '../shared/queries/users'
 import {getAccountOwners} from '../shared/services/users'
-
+import DropDown from '../shared/components/dropDown'
+import {Map} from 'immutable'
 import {
   networkModelRenderer,
   queryRenderer,
@@ -10,10 +11,19 @@ import {
 } from '../shared/components/networkRenderer'
 
 class AccountOwnersView extends React.Component {
+  getMenuItems() {
+    return this.props.owners.map(o => new Map({
+      label: o.get('name'),
+      value: o.get('id')
+    })).unshift(new Map({label: "", value: ""}));
+  }
   render() {
-    return <DropDown {...this.props} selectedValue={owner.id} menuItems={this.props.owners} />
+    return <DropDown {...this.props}
+                     selectedValue={this.props.owner.id}
+                     menuItems={this.getMenuItems()} />
   }
 }
+AccountOwnersView.displayName = 'AccountOwnersInternal';
 
 let AccountOwners = queryRenderer(AccountOwnersView, {
   queries: [
@@ -26,5 +36,7 @@ let AccountOwners = queryRenderer(AccountOwnersView, {
     }
   ]
 });
+
+AccountOwners.displayName = 'AccountOwners';
 
 export default AccountOwners;
