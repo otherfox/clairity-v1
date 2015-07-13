f756d369107ace8776cf30f74e2b314f041f9c74
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-13 15:03:07 -0400

    Deletes queries which aren't needed because of collectionQuery

--- src/shared/queries/projectType.js
+++
@@ -1,8 +0,0 @@
-
-import _ from 'lodash'
-import Store from '../store'
-
-export function queryAllProjectTypes() {
-  let result = Store.data.get('projectType').toList();
-  return result.size > 0 ? result : null;
-}
--- src/shared/queries/salesStage.js
+++
@@ -1,8 +0,0 @@
-
-import _ from 'lodash'
-import Store from '../store'
-
-export function queryAllSalesStages() {
-  let result = Store.data.get('salesStage').toList();
-  return result.size > 0 ? result : null;
-}
