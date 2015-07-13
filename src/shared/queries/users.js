67292bac3c88b3a60f4c03e794a07e877396ecad
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-13 15:01:24 -0400

    todo note in users query

--- src/shared/queries/users.js
+++ src/shared/queries/users.js
@@ -2,6 +2,7 @@
 import Store from '../store'
 
 export function queryAccountOwners() {
+  // TODO: Verify that this works the way I think it does
   let result = Store.data.get('user').toList();
   return result.size > 0 ? result : null;
 }
