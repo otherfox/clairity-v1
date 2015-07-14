367eb6f0d6ee59c2743e45a053469859a0b24260
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-14 01:38:36 -0400

    Actions actually posts to controller.cfm?event=convertLead now

--- src/createOpportunity/actions.js
+++ src/createOpportunity/actions.js
@@ -1,8 +1,11 @@
 import Fynx from 'fynx'
 import Store from '../shared/store'
 
+import {postConvertLead} from '../shared/services/contacts'
+
 export const convertLead = Fynx.createAsyncAction();
 
 convertLead.listen(leadInfo => {
   console.log('converting lead...', leadInfo)
+  postConvertLead(leadInfo);
 });
