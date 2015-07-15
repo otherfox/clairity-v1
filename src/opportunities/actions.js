2843aaf01aa54727fedb4f67f19bf88ab8c52566
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-15 05:01:02 -0400

    Moved opps actions

---
+++ src/opportunities/actions.js
@@ -0,0 +1,16 @@
+import Fynx from 'fynx'
+import Store from '../../shared/store'
+
+import { postConvertLead } from '../shared/services/contacts'
+import { putOpportunity } from '../shared/services/opportunity'
+
+export const convertLead = Fynx.createAsyncAction();
+export const updateSalesOpp = Fynx.createAsyncAction();
+
+convertLead.listen(leadInfo => {
+  postConvertLead(leadInfo.data).then(a => leadInfo.completed());
+});
+
+updateSalesOpp.listen(oppInfo => {
+  putOpportunity(oppInfo.data).then(a => oppInfo.completed && oppInfo.completed(a));
+});
