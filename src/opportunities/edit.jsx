1ec2d275afb317b97684a306177770b7cdeae89f
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-15 02:26:45 -0400

    Opps edit page stubbed

--- src/opportunities/edit.jsx
+++ src/opportunities/edit.jsx
@@ -4,155 +4,54 @@ import DropDown from '../shared/components/dropDown'
 import Details from  '../shared/components/details'
 import Header from '../shared/components/header'
 import {
-  RadioButtonGroup,
-  RadioButton,
   Checkbox,
-  FlatButton,
   RaisedButton,
-  FloatingActionButton,
-  IconButton,
-  Toggle,
-  Slider,
-  DropDownMenu,
-  DatePicker,
   TextField,
   Paper
 } from 'material-ui'
 
-import controllable from 'react-controllables'
-import {List} from 'immutable'
-import {Navigation} from 'react-router'
+import { List, Map } from 'immutable'
+import { State, Link } from 'react-router'
 
-let editLead = React.createClass({
-  mixins: [Navigation],
+import EditAccountDetails from '../accounts/parts/editDetails'
+import EditDetails from './parts/editDetails'
+
+/*
+let event = 'controller.cfm?event=updateSalesOpp';
+let hiddenValues = {
+  customer_id: "1480",
+  oportunity_id: "7223",
+  tax_exempt: false,
+  summary_billing: false,
+  show_international: true,
+  show_long_distance: false,
+  email_invoice: false,
+  invoice_weekly: false,
+  vip: false,
+  auto_pay: false,
+};
+*/
+
+let editOpportunity = React.createClass({
+  mixins: [State],
 
   render() {
-    let event = 'controller.cfm?event=updateSalesOpp';
 
-    let hiddenValues = {
-      customer_id: "1480",
-      oportunity_id: "7223",
-      tax_exempt: false,
-      summary_billing: false,
-      show_international: true,
-      show_long_distance: false,
-      email_invoice: false,
-      invoice_weekly: false,
-      vip: false,
-      auto_pay: false,
-    };
-
-    let opportunity = { name: 'Central Regional Wastewater System'};
+    let opp = { name: 'Central Regional Wastewater System'};
 
     return (
       <Layout widths={{}} cPadding={'20px 20px 0 0'}>
-        <Header><h1>Edit Lead - {opportunity.name}</h1></Header>
-      <Paper>
-        <Layout widths={{ lg: [6,6]}} cPadding={'0 20px 20px 20px'}>
-          <Details
-            cStyles={{ lg: [{textAlign: 'left'}]}}
-            rowStyle={{marginLeft: '15%'}}
-            widths={{ lg: [4,8]}}
-            title={'Customer Details'}
-            data={[
-              { label: 'Current Account Owner', value: <TextField value={'Kit Carker'} disabled={true} />, detailType: 'muiTextField' },
-              { label: 'Name', name: 'name', value: <TextField value={''}/>, detailType: 'muiTextField' },
-              { label: 'Type', name: 'customerTypeId', value: <DropDown selectedValue={0} menuItems={ new List([
-                { label: '', value: 0},
-                { label: 'Business', value: 1 },
-                { label: 'Residential', value: 2 },
-                { label: 'Telenational - Business', value: 3 },
-                { label: 'Telenational - Residential', value: 4 }
-              ])} />, detailType: 'muiDropDown' },
-            ]}
-          />
-          <Details
-            cStyles={{ lg: [{textAlign: 'left'}]}}
-            widths={{ lg: [2,10]}}
-            title={null}
-            data={[
-              { label: 'Street 1', name: 'customerStreet1', value: <TextField value={'15400 Knoll Trail'}/>, detailType: 'muiTextField' },
-              { label: 'Street 2', name: 'customerStreet2', value: <TextField value={'Suite 400'}/>, detailType: 'muiTextField' },
-              { label: 'City', name: 'customerCity', value: <TextField value={'Dallas'} />, detailType: 'muiTextField' },
-              { label: 'State', name: 'customerState', value: <TextField value={'TX'} />, detailType: 'muiTextField' },
-              { label: 'Zip Code', name: 'customerZip', value: <TextField value={75248} />, detailType: 'muiTextField' }
-            ]}
-          />
+        <Layout widths={{lg:[8,4], sm:[12,12]}} cPadding={'0 20px 0 0'}>
+          <Header><h1>Edit Opportunity - {opp.name}</h1></Header>
+          <Link to="view-opp" params={this.getParams()}>
+            <RaisedButton style={{float: 'right', marginTop: 25}} primary label="Return" />
+          </Link>
         </Layout>
-      </Paper>
-      <Paper>
-        <Layout widths={{ lg: [6,6] }} cPadding={'0 20px 20px 20px'}>
-          <Details
-            cStyles={{ lg: [{textAlign: 'left'}]}}
-            rowStyle={{marginLeft: '15%'}}
-            widths={{ lg: [4,8]}}
-            title={'Opportunity Details'}
-            data={[
-              { label: 'Name', name: 'oppName', value: <TextField value={'Fill the Bill'}/>, detailType: 'muiTextField' },
-              { label: 'Stage', name: 'salesStageId', value: <DropDown selectedValue={0} menuItems={ new List([
-                { label: '', value: 0},
-                { label: 'Closed Won', value: 5 },
-                { label: 'Clost Lost', value: 6 },
-                { label: 'Needs Analysis', value: 2 },
-                { label: 'Negotiation', value: 4 },
-                { label: 'Proposal', value: 3 },
-                { label: 'Qualification', value: 1 },
-              ])} />, detailType: 'muiDropDown' },
-              { label: 'Project Type', name: 'salesProjectTypeId', value: <DropDown selectedValue={0} menuItems={ new List([
-                { label: '', value: 0},
-                { label: 'Fiber Data (Existing)', value: 6 },
-                { label: 'Fiber Data (New)', value: 2 },
-                { label: 'Fiber Voice &amp; Data (Existing)', value: 8 },
-                { label: 'Fiber Voice &amp; Data (New)', value: 4 },
-                { label: 'Wireless Data (Existing)', value: 5 },
-                { label: 'Wireless Data (New)', value: 1 },
-                { label: 'Wireless Voice &amp; Data (Existing)', value: 7 },
-                { label: 'Wireless Voice &amp; Data (New)', value: 3 }
-              ])} />, detailType: 'muiDropDown' },
-              { label: 'Lead Source', name: 'salesLeadSrcId', value: <DropDown selectedValue={0} menuItems={ new List([
-                { label: '', value: 0},
-                { label: 'Advertisement', value: 1 },
-                { label: 'Email', value: 10 },
-                { label: 'Employee Referal', value: 2 },
-                { label: 'External Referal', value: 3 },
-                { label: 'Other', value: 12 },
-                { label: 'Partner', value: 4 },
-                { label: 'Public Relations', value: 5 },
-                { label: 'Seminar - External', value: 7 },
-                { label: 'Seminar - Internal', value: 6 },
-                { label: 'Trade Show', value: 8 },
-                { label: 'Web', value: 9 },
-                { label: 'Word of Mouth', value: 11 },
-              ])} />, detailType: 'muiDropDown' },
-              { label: 'Lead Campaign Source', name: 'salesCampSrcId', value: <DropDown selectedValue={0} menuItems={ new List([
-                { label: '', value: 0},
-                { label: 'Conference - 2015', value: 2 },
-                { label: 'Oz', value: 1 },
-                { label: 'Spring Webinar - 2015', value: 3 },
-              ])} />, detailType: 'muiDropDown' },
-              { label: '', names: ['offer_made', 'project_started'], value: <Layout widths={{lg: [6,6], sm: [12]}}><Checkbox name={'offer_made'} value={1} label={'Offer Made'} defaultSwitched={true} switched/><Checkbox name={'project_started'} value={1} label={'Project Started'} defaultSwitched={true} switched/></Layout>, detailType: 'muiCheckbox'}
-            ]}
-          />
-          <Details
-            cStyles={{ lg: [{textAlign: 'left'}]}}
-            widths={{ lg: [4,8]}}
-            title={null}
-            data={[
-              { label: 'Date Offer Made', name: 'dt_offer_made', value: <TextField value={''}/>, detailType: 'muiTextField' },
-              { label: 'Date Project Started', name: 'dt_project_start', value: <TextField value={''}/>, detailType: 'muiTextField' },
-              { label: 'Closing Date', name: 'dt_closing', value: <TextField value={''}/>, detailType: 'muiTextField' },
-              { label: 'Project Ending Date', name: 'dt_project_end', value: <TextField value={''}/>, detailType: 'muiTextField' },
-              { label: 'Sales Amount', name: 'sales', value: <TextField value={''}/>, detailType: 'muiTextField' },
-              { label: 'Probability Pct.', name: 'probability', value: <TextField value={''}/>, detailType: 'muiTextField' },
-              { label: '', name: 'project_result', value: <Checkbox name={'project_result'} value={1} label={'Project Successful'} defaultSwitched={false} switched/>, detailType: 'muiCheckbox' },
-              { label: '', value: <RaisedButton primary label="Update" />, detaildetailType: 'muiButton'}
-            ]}
-          />
-        </Layout>
-      </Paper>
-    </Layout>
+        <EditAccountDetails />
+        <EditDetails opportunity={new Map(opp)} />
+      </Layout>
     );
   }
 });
 
-export default editLead;
+export default editOpportunity;
