a6296c262c325d22727ced02632f7dbe28a161ea
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-15 06:20:57 -0400

    Created view contact page stub

---
+++ src/contacts/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import {Link} from 'react-router'
+
+export default class ViewContact extends React.Component {
+  render() {
+    return (
+      <div>Contact page stub</div>
+    );
+  }
+}
