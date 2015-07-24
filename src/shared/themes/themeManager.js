54390889cc5fd1cea9a3f523bae56047d9f49798
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-24 15:10:10 -0400

    ThemeManager no longer sets it's own default theme

--- src/shared/themes/themeManager.js
+++ src/shared/themes/themeManager.js
@@ -1,9 +1,5 @@
-import ClairityLightTheme from './clairityLight.js'
 import {Styles} from 'material-ui'
 
 let ThemeManager = new Styles.ThemeManager();
-let ClairityDarkTheme = ThemeManager.types.DARK;
-
-ThemeManager.setTheme(ClairityLightTheme);
 
 export default ThemeManager;
