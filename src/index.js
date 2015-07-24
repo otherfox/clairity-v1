683bb43f99adece7c5bad66560e4450cd4dc8eb1
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-24 17:24:21 -0400

    Main file imports settings now

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {} from 'babel/polyfill'
 import {} from './less/main'
 import React, {addons} from 'react/addons'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import SettingsManager from 'shared/settings'
 
 import Router from './shared/router'
 
