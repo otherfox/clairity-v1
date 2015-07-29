47fd0b7478640b79c096745b0796505ad388de67
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-29 16:29:30 -0400

    Basic naive stack implementation for link

--- src/shared/components/link.jsx
+++ src/shared/components/link.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {State, Link} from 'react-router'
 import _ from 'lodash'
+import {navStack} from '../router/navigationStack'
 
 class CustomLink extends React.Component {
 
@@ -12,9 +13,17 @@ class CustomLink extends React.Component {
     }
   }
 
+  get self() {
+    return this.refs.self.props;
+  }
+
+  handleNavigation() {
+    navStack.push(this.props.to, this.props.params, this.props.displayName);
+  }
+
   render() {
     return (
-      <Link onClick={() => alert('link clicked!')} to={this.props.to} params={this.props.params} style={_.assign(this.style(), this.props.style)}>
+      <Link ref="self" onClick={() => this.handleNavigation()} to={this.props.to} params={this.props.params} style={_.assign(this.style(), this.props.style)}>
         {this.props.children}
       </Link>
     );
