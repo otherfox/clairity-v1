9d09a9fc2beb46960bb493deaa957c85785da6f9
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-30 01:04:50 -0400

    Create opp form

--- src/opportunities/create/form.jsx
+++ src/opportunities/create/form.jsx
@@ -1,6 +1,13 @@
 import React, {PropTypes, addons} from 'react/addons'
 import controllable from 'react-controllables'
 
+import { collectionDropdown } from '../../shared/components/collectionDropdown'
+
+let ProjectTypes = collectionDropdown('projectType');
+let SalesStages = collectionDropdown('salesStage');
+let LeadSources = collectionDropdown('leadSource');
+let CampaignSources = collectionDropdown('campaignSource');
+
 @controllable(['name', 'project_type', 'stage', 'lead_source', 'lead_source_id'])
 class CreateFormComponent extends React.Component {
   render() {
@@ -13,13 +20,13 @@ class CreateFormComponent extends React.Component {
               title={`Create Opportunity with ${opp.name} @ ${opp.company}`}
               headerStyle={{color: '#aaa', marginLeft: '32%', marginBottom: '30px'}}
               data={[
-                { label: 'Opportunity Name', name: 'name', value: <TextField value={this.props.name} onChange={e => this.props.onNameChange(e.target.value)}/>, detailType: 'muiTextField' },
+                { label: 'Opportunity Name', value: <TextField value={this.props.name} onChange={e => this.props.onNameChange(e.target.value)}/>, detailType: 'muiTextField' },
                 { label: 'Current Account Owner:', value: <TextField value={owner.name} disabled={true} />, detailType: 'muiTextField' },
-                { label: 'Project Type', name: 'project_type', value: <ProjectTypes valueLink={this.linkState('project_type')}/>, detailType: 'muiDropDown' },
-                { label: 'Stage', name: 'stage', value: <SalesStages valueLink={this.linkState('stage')} />, detailType: 'muiDropDown' },
-                { label: 'Lead Source', name: 'lead_source_id', value: <LeadSources valueLink={this.linkState('lead_source_id')} />, detailType: 'muiDropDown' },
-                { label: 'Lead Campaign Source', name: 'lead_source', value: <CampaignSources valueLink={this.linkState('lead_source')} />, detailType: 'muiDropDown' },
-                { label: '', value: <div><RaisedButton primary={true} label="Update" onClick={this.convertLead} /> <Link style={{paddingLeft: '20px'}} to="view-account" params={{locationId: 1, accountId: opp.customer_id}}>View Account</Link></div>, detaildetailType: 'muiButton'}
+                { label: 'Project Type', value: <ProjectTypes value={this.props.project_type} onChange={v => this.props.onProject_typeChange(v)}/>, detailType: 'muiDropDown' },
+                { label: 'Stage', value: <SalesStages value={this.props.stage} onChange={v => this.props.onStageChange(v)} />, detailType: 'muiDropDown' },
+                { label: 'Lead Source', value: <LeadSources value={this.props.lead_source_id} onChange={v => this.props.onLead_source_idChange(v)} />, detailType: 'muiDropDown' },
+                { label: 'Lead Campaign Source', value: <CampaignSources value={this.props.lead_source} onChange={v => this.props.onLead_sourceChange(v)} />, detailType: 'muiDropDown' },
+                { label: '', value: <div><RaisedButton primary={true} label="Update" onClick={() => this.props.onSubmit(this.props)} /> <Link style={{paddingLeft: '20px'}} to="view-account" params={{locationId: 1, accountId: opp.customer_id}}>View Account</Link></div>, detaildetailType: 'muiButton'}
               ]}
             />
           </Layout>
