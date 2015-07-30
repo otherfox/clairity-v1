928e8bfe5d3e5972b562194060b84bf95d58aab5
Nicholas Tennies <nick@brainbytescreative.com>
2015-07-30 12:16:07 -0400

    fixes resources in details

--- src/shared/components/details/index.jsx
+++ src/shared/components/details/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Settings from './settings'
+import Settings from '../settings'
 import _ from 'lodash'
 import {
   Utils,
@@ -7,7 +7,7 @@ import {
   ClearFix
 } from 'material-ui'
 
-import Layout from './layout'
+import Layout from '../layout'
 
 let ColorManipulator = Utils.ColorManipulator;
 
