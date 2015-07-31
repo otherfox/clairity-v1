492dec2ad25e3d05c2354e960c2b2d6d62389c45
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-07-31 15:36:18 -0400

    Created a demo of how filtered collection should work

---
+++ src/shared/components/filteredCollection/filters.jsx
@@ -0,0 +1,13 @@
+// class Thing extends React.Component {
+//   render() {
+//     <FilteredCollection data={someData}>
+//       <Filters>
+//         <Filter type="text" field="name" />
+//         <Filter type="typeahead" field="address" />
+//         <Filter type="checkbox" field="active" />
+//         <Filter type="radio" field="gender" options=['Male', 'Female', '???'] />
+//       </Filters>
+//       <Table {...tableConfig} />
+//     </FilteredCollection>
+//   }
+// }
