0776a3e1560655ca6e94e0006709f6aa563b40da
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-05 00:41:31 -0400

    Added example impl for bridge to worker

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,6 +1,33 @@
+
 try {
   !!window;
   console.log('outside worker');
 } catch (e) {
   console.log('inside worker!');
 }
+
+/*
+
+// Example implemention of bridge/hub for Worker
+
+import {v4} from 'uuid'
+
+const callbackMap = new Map();
+
+let worker = {postMessage(){}}
+
+worker.onmessage = msg => {
+  let token = msg.token;
+  callbackMap.get(token)(msg);
+  callbackMap.delete(token);
+};
+
+
+function getPromiseForMessage(message) {
+  return new Promise((resolve, reject) => {
+    let token = Symbol(v4());
+    message.token = token;
+    callbackMap.put(token, resolve);
+  });
+}
+*/
