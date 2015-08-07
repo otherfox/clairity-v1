bbd6947fc52e915f142e5a46d6e26809fbe1d49d
Nicholas Tennies <nick@brainbytescreative.com>
2015-08-07 15:11:55 -0400

    makes filters layout adjustable

--- src/shared/components/filteredCollection/filters.jsx
+++ src/shared/components/filteredCollection/filters.jsx
@@ -1,18 +1,19 @@
 import React, {Component} from 'react'
 import Filter from './filter'
 import Layout from '../layout'
+import {Paper} from 'material-ui'
 import _ from 'lodash'
 import {throttle} from 'throttle-debounce'
 
 class Filters extends Component {
   style() {
     return {
-      filter: {
-        float: 'left',
-        maxWidth: '360px',
-        width: '100%',
-        marginRight: '30px',
-        marginBottom: '10px'
+      lg: {
+        maxWidth: '380px'
+      },
+      md: {
+        maxWidth: '100%',
+        color: 'red'
       }
     }
   }
@@ -29,16 +30,31 @@ class Filters extends Component {
   }
 
   render() {
-    let children = React.Children.map(this.props.children, (child, idx) =>
-      React.addons.cloneWithProps(child, {
-        style: _.assign(this.style().filter, child.props.style),
-        onChange: throttle(250, false, this.props.onChange, false),
-        ref: `filter${idx}`
-      }));
+    let layoutProps = { lgStyles: [], mdStyles: [], widths: []};
+    let children = React.Children.map(this.props.children, (child, idx) => {
+      layoutProps.lgStyles.push(this.style().lg);
+      layoutProps.mdStyles.push(this.style().md);
+      layoutProps.widths.push(12);
+      return ( <div>
+        {React.addons.cloneWithProps(child, {
+          style: _.assign( { width: '100%', maxWidth: '360px' }, child.props.style),
+          onChange: throttle(250, false, this.props.onChange, false),
+          ref: `filter${idx}`
+        })}
+      </div> )}
+    );
     return (
-      <Layout widths={{}}>{children}</Layout>
+      <div>
+        <Layout widths={_.assign({ md: layoutProps.widths }, this.props.widths)} cStyles={_.assign({ lg: layoutProps.lgStyles, md: layoutProps.mdStyles }, this.props.cStyles)} cPadding={this.props.cPadding} breakpoints={_.assign({ md: 1400 }, this.props.breakpoints)}>
+          {children}
+        </Layout>
+      </div>
     )
   }
+}
+
+Filters.defaultProps = {
+  cPadding: '0 20px 10px 0'
 }
 
 export default Filters;
