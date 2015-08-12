38fb0f4847e3c4b7f17a87cddf8e741cc140ec29
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-12 03:34:38 -0400

    Updated work order messages service

--- src/shared/services/messages.js
+++ src/shared/services/messages.js
@@ -3,7 +3,11 @@ import moment from 'moment'
 
 import req from 'superagent'
 
-export function getWorkOrderMessagesByWorkOrder(id) {
+import { withDelay } from 'memoize-promise'
+
+const memoize = withDelay(10000); // ten second delay
+
+let getWorkOrderMessagesByWorkOrder = memoize(id => {
   return new Promise((s, f) => {
     req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.WorkOrderMessagesDAO&_m=getAllWorkOrderMessagesByWorkOrderId&work_order_id=${id}`)
       .withCredentials()
@@ -15,4 +19,6 @@ export function getWorkOrderMessagesByWorkOrder(id) {
         }
       });
   });
-}
+});
+
+export { getWorkOrderMessagesByWorkOrder };
