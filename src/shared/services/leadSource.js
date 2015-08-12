b2effdfdc82f683a34b1f2721dfca35e4ec349d9
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-12 03:27:28 -0400

    Updated lead sources services

--- src/shared/services/leadSource.js
+++ src/shared/services/leadSource.js
@@ -3,7 +3,11 @@ import moment from 'moment'
 
 import req from 'superagent'
 
-export function getLeadSources() {
+import { withDelay } from 'memoize-promise'
+
+const memoize = withDelay(10000); // ten second delay
+
+let getLeadSources = memoize(() => {
   return new Promise((s, f) => {
     req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.SalesLeadSrcDAO&_m=getAllSalesLeadSrcs`)
       .withCredentials()
@@ -15,4 +19,6 @@ export function getLeadSources() {
         }
       });
   });
-}
+});
+
+export { getLeadSources };
