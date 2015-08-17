99b36e19e1c27fe91995656a26573fa43ea3c561
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-17 13:11:01 -0400

    Updated async wrapper

--- src/ui/shared/components/asyncWrapper/asyncWrapper.jsx
+++ src/ui/shared/components/asyncWrapper/asyncWrapper.jsx
@@ -13,6 +13,16 @@ export default function asyncWrapper() {
   const Component = args[0];
   const options = args[1];
 
+  const actions = _.chain(options)
+                   .pairs()  // convert to arrays to maintain key names
+                   .filter(p => p[1].type === 'action')
+                   .reduce((s, p) => s[p[0]] = p[1], {});
+
+  const queries = _.chain(options)
+                   .pairs()
+                   .filter(p => p[1].type === 'query')
+                   .reduce((s, p) => s[p[0]] = p[1], {});
+
   class AsyncWrapper extends React.Component {
 
     constructor(props) {
