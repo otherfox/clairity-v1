ca0b3e05a413b10cfc5e2b82c3e564884b9d3130
Nick Tennies <nick@brainbytescreative.com>
2015-08-17 12:17:37 -0400

    renames tickets to ticket

---
+++ src/shared/services/tickets.js
@@ -0,0 +1,34 @@
+import _ from 'lodash'
+import moment from 'moment'
+
+import {debug} from '../mixins/debug'
+
+import req from 'superagent'
+
+export function getTicket(id) {
+  return new Promise((s, f) => {
+    req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.TicketDAO&_m=getTicketById&ticket_id=${id}`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
+
+export function getTickets() {
+  return new Promise((s, f) => {
+    req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.TicketDAO&_m=getAllTickets`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          s(JSON.parse(res.text));
+        } else {
+          f(err);
+        }
+      });
+  });
+}
