263e2bf3879b187ffe9d01de84dfbf65242a2ba7
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-18 16:05:09 -0400

    Re-wrote handlers to work with webpack exports

--- src/core/handlers.js
+++ src/core/handlers.js
@@ -1,8 +1,10 @@
-
-export function query(info) {
-  console.log('query request received', info);
-}
-
-export function action(info) {
-  console.log('action request received', info);
+export default {
+  query(info) {
+    console.log('query request received', info);
+    return Promise.resolve(info);
+  },
+  action(info) {
+    console.log('action request received', info);
+    return Promise.resolve(info);
+  }
 }
