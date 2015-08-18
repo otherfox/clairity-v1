5960dd62933335195c05ed3575ec91f695cc8867
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-18 16:05:32 -0400

    Worker message handler actually delivers messages to handlers now

--- src/core/index.js
+++ src/core/index.js
@@ -3,5 +3,5 @@ import Handlers from './handlers'
 
 onmessage = function handleMessage(message) {
   console.log('worker thread', 'got message', message.data);
-  Handlers[message.data.type].then(postMessage);
+  Handlers[message.data.type](message.data).then(postMessage);
 };
