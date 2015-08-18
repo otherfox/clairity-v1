ceade9e2d2b0a35a814daea32ca61c6ab0eacaeb
Nicholas Tennies <nick@brainbytescreative.com>
2015-08-18 11:42:57 -0400

    fixes footer

--- src/shared/components/footer.jsx
+++ src/shared/components/footer.jsx
@@ -5,36 +5,49 @@ import {Utils, Styles} from 'material-ui'
 let ColorManipulator = Utils.ColorManipulator;
 
 class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: 'relative'
+    }
+  }
 
-  style() {
+  componentDidMount() {
+    this.getPosition();
+    window.addEventListener('resize', e => this._getPosition());
+  }
 
+  componentWillDismount() {
+    window.removeEventListener('resize', e => this._getPosition());
+  }
+
+  render() {
+    return (
+      <div style={this._style()}>
+        Copywrite 2015 One Ring Networks
+      </div>
+    );
+  }
+
+  _style() {
     let textColor = Styles.Colors.white;
     let backgroundColor = Styles.Colors.black;
 
     return {
       padding: '20px',
       textAlign: 'center',
-			position: 'absolute',
-    	width: '100%',
+      position: this.state.position,
+      width: '100%',
       height: Settings.footerHeight+'px',
       color: textColor,
       backgroundColor: backgroundColor,
-    	bottom: '0',
+      bottom: '0',
       zIndex: 2
     }
   }
 
-  shouldComponentUpdate() {
-    return false;
-  }
-
-  render() {
-
-    return (
-      <div style={this.style()}>
-        Copywrite 2015 One Ring Networks
-      </div>
-    );
+  _getPosition() {
+    this.setState({ position: (window.innerHeight > Settings.contentMinHeight + 122) ? 'fixed' : 'relative'});
   }
 }
 
