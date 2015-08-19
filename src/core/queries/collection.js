625aef30efad51f8f97e2fe0ecaaf522b845e482
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-19 10:28:34 -0400

    Adds table.toLowerCase so queries aren't case sensitive

--- src/core/queries/collection.js
+++ src/core/queries/collection.js
@@ -1,13 +1,13 @@
 
 export function collection(args) {
   let { table } = args;
-  let result = Store.data.get(table);
+  let result = Store.data.get(table.toLowerCase());
   return result.size > 0 ? result : null;
 }
 
 export function collectionVia(args) {
   let { table, filterId, idName } = args;
-  let result = Store.data.get(table)
+  let result = Store.data.get(table.toLowerCase())
                          .toList()
                          .filter(r => r.get(idName) == filterId);
   return result.size > 0 ? result : null;
