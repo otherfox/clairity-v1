f2a95e147d914f4d1a78b716634716f2c2b2ff70
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-19 21:02:59 -0400

    Worker re-attaches tokens when replying to messages

--- src/core/index.js
+++ src/core/index.js
@@ -3,5 +3,8 @@ import Handlers from './handlers'
 
 onmessage = function handleMessage(message) {
   console.log('worker thread', 'got message', message.data);
-  Handlers[message.data.type](message.data).then(postMessage);
+  Handlers[message.data.type](message.data).then(data => {
+    data.token = message.data.token;
+    postMessage(data);
+  });
 };
