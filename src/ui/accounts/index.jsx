9efac8376240a07a2c6cc7904fbd6901a1e4ce11
Nicholas Tennies <nick@brainbytescreative.com>
2015-08-21 18:58:19 -0400

    starts styling account view

--- src/ui/accounts/index.jsx
+++ src/ui/accounts/index.jsx
@@ -87,9 +87,9 @@ let accountView = React.createClass({
               :
                 <AccountDetails user={null} account={this.props.account} />
           }
-          <Paper style={{padding: '10px 20px 20px 20px'}}><h3 style={{marginBottom: '10px'}}>Opportunities</h3><OppsListQuery accountId={account.id} /></Paper>
-          <Paper style={{padding: '10px 20px 20px 20px'}}><h3 style={{marginBottom: '10px'}}>Contacts</h3><ContactListQuery accountId={account.id} /></Paper>
-          <Paper style={{padding: '10px 20px 20px 20px'}}><h3 style={{marginBottom: '10px'}}>Contracts</h3><ContractsListQuery accountId={account.id} /></Paper>
+          <div><Paper style={{padding: '10px 20px 20px 20px'}}><h3 style={{marginBottom: '10px'}}>Opportunities</h3><OppsListQuery accountId={account.id} /></Paper></div>
+          <div><Paper style={{padding: '10px 20px 20px 20px'}}><h3 style={{marginBottom: '10px'}}>Contacts</h3><ContactListQuery accountId={account.id} /></Paper></div>
+          <div><ContractsListQuery accountId={account.id} /></div>
         </Layout>
 
       </Layout>
