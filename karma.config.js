aa7e1aaf9394c6aa2c220236070fa2f1d93e8a03
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-25 15:58:02 -0400

    Karma adds sinon to framework list

--- karma.config.js
+++ karma.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = function(config) {
   return config.set({
     basePath: '',
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'sinon'],
     files: ['test/spec/**/*.js'],
     plugins: [
       require('karma-webpack'),
