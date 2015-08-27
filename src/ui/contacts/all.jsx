281014aa398680b1868af6b670b5eed283c80afe
Nicholas Tennies <nick@brainbytescreative.com>
2015-08-27 17:34:34 -0400

    contacts load into all contacts

---
+++ src/ui/contacts/all.jsx
@@ -0,0 +1,44 @@
+import React, { PropTypes, Component } from 'react'
+import async, { collection } from '../shared/components/async'
+import { contextTypes } from '../shared/decorators'
+import Link from '../shared/components/link'
+import { FontIcon, ClearFix } from 'material-ui'
+
+@async({ contacts: collection('contact').all() })
+@contextTypes({ muiTheme: PropTypes.object })
+class ListContacts extends Component {
+  style() {
+      return {
+        root: {},
+        icon: {
+          float: 'left',
+          color: this.context.muiTheme.palette.primary1Color
+        },
+        link: {
+          float: 'left',
+          color: this.context.muiTheme.palette.textColor,
+          lineHeight: '25px',
+          marginLeft: '10px'
+        }
+      }
+  }
+  render() {
+    console.log(this.props.contacts);
+    return (
+      <div style={this.style().root}>
+        <h1>Contacts</h1>
+        {
+          this.props.contacts.map(o =>
+            <ClearFix>
+              <Link to="view-contact" params={{contactId: o.id}}>
+                <FontIcon className={'md md-account-circle'} style={this.style().icon}/> <div style={this.style().link}>{o.name}</div>
+              </Link>
+            </ClearFix>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default ListContacts;
