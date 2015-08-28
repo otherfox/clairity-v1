450c10b2facccdaf64f0ac10fc686c41efeb683b
Nick Tennies <nick@brainbytescreative.com>
2015-08-28 13:30:37 -0400

    makes createTickets

---
+++ src/ui/tickets/create.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react'
+
+class CreateTicket extends Component {
+  render() {
+    return (
+      <div>
+        <h1>create ticket</h1>
+      </div>
+    )
+  }
+}
+
+export default CreateTicket
