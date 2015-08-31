2d59c45d1c2c545ceaede9006a1ecc1c12f818fc
Nick Tennies <nick@brainbytescreative.com>
2015-08-31 12:02:35 -0400

    adds prorieties statuses and templates to ticket services

--- src/core/services/ticket.js
+++ src/core/services/ticket.js
@@ -20,9 +20,56 @@ export function getTicket(id) {
 // TODO: Added param getActive to true. Need to add more services etc.
 
 export function getTickets() {
-  console.log('service tickets')
   return new Promise((s, f) => {
     req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.TicketDAO&_m=getAllTicketHeaders&getActive=true`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          console.log('response:',res);
+          s(JSON.parse(res.text));
+        } else {
+          console.log('error:',err);
+          f(err);
+        }
+      });
+  });
+}
+
+export function getTicketsTemplates() {
+  return new Promise((s, f) => {
+    req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.TicketTemplateDAO&_m=getAllTicketTemplates`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          console.log('response:',res);
+          s(JSON.parse(res.text));
+        } else {
+          console.log('error:',err);
+          f(err);
+        }
+      });
+  });
+}
+
+export function getTicketsStatuses() {
+  return new Promise((s, f) => {
+    req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.TicketDAO&_m=getAllTicketStatuses`)
+      .withCredentials()
+      .end((err, res) => {
+        if (!err) {
+          console.log('response:',res);
+          s(JSON.parse(res.text));
+        } else {
+          console.log('error:',err);
+          f(err);
+        }
+      });
+  });
+}
+
+export function getTicketsPriorities() {
+  return new Promise((s, f) => {
+    req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.TicketDAO&_m=getAllTicketPriorities`)
       .withCredentials()
       .end((err, res) => {
         if (!err) {
