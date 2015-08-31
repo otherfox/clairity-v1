50bc9ed3e1efc7bda60cc6152e444324d5557367
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-31 14:46:40 -0400

    Create contact form submits dummy object now

--- src/ui/contacts/create.jsx
+++ src/ui/contacts/create.jsx
@@ -1,11 +1,27 @@
 import React, { PropTypes, Component } from 'react'
 import async, { action } from '../shared/components/async'
 
+import { RaisedButton } from 'material-ui'
+
 @async({ createContact: action() })
 class CreateContact extends Component {
   render() {
     return (
-      <div />
+      <RaisedButton onClick={() => this.props.actions.createContact({
+        company: '',
+        name: '',
+        comments: '',
+        customer_id: 0,
+        phone: '',
+        cell: '',
+        id: '',
+        email: '',
+        relationships: [{
+          id: '', customer_id: 0, contact_id: 0, type: ''
+        }],
+        extension: '',
+        fax: ''
+      })} label="Submit" />
     );
   }
 }
