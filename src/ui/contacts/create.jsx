31094bade497c1aa5248e20f5b068f550d24bc87
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-01 10:30:53 -0400

    Creating a contact actually works now

--- src/ui/contacts/create.jsx
+++ src/ui/contacts/create.jsx
@@ -8,17 +8,17 @@ class CreateContact extends Component {
   render() {
     return (
       <RaisedButton onClick={() => this.props.actions.createContact({
-        company: '',
-        name: '',
-        comments: '',
-        phone: '',
-        cell: '',
-        email: '',
+        company: 'form test 1',
+        name: 'test test',
+        comments: 'test',
+        phone: '111-111-1111',
+        cell: '222-222-2222',
+        email: 'test@test.test',
         relationships: [{
           pop_id: 0, customer_id: 0, type: 0
         }],
-        extension: '',
-        fax: ''
+        extension: 'extensions',
+        fax: 'fax'
       })} label="Submit" />
     );
   }
