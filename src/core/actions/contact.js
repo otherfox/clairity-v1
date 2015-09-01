397141d63e7121fe68f31f19a73ddd379d735268
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-31 21:42:01 -0400

    Implements create contact action

--- src/core/actions/contact.js
+++ src/core/actions/contact.js
@@ -1,4 +1,18 @@
 
-export function createContact(params) {
-  return new Promise(() => 0);
+import {
+  postInsertContact,
+  postInsertContactRelationship
+} from '../services/contact'
+
+export function createContact(contact) {
+  let firstPromise = postInsertContact(contact);
+  let returnPromise = new Promise(impl);
+  function impl(resolve, reject) {
+    firstPromise.then(contactId => {
+      Promise.all(contact.relationships.map(r =>
+        postInsertContactRelationship(r.contact_id = contactId && r)
+      )).then(resolve, reject);
+    });
+  }
+  return returnPromise;
 }
