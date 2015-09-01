bbf942c17d37d775674a2d470e2859b60a6264a5
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-08-31 21:24:53 -0400

    Adds contact gateway stubs

--- src/core/gateways/contact.js
+++ src/core/gateways/contact.js
@@ -2,3 +2,11 @@
 export function eventConvertLead(contact) {
   return contact;
 }
+
+export function eventInsertContact(contact) {
+  return contact;
+}
+
+export function eventInsertContactRelationship(rel) {
+  return rel;
+}
