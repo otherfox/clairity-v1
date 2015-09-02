5d82860867c63cd78ec6c1a07b5b83b925dbf65f
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-02 15:57:05 -0400

    dev-server.js again

--- dev-server.js
+++ dev-server.js
@@ -1,6 +1,6 @@
-var config = require('./webpack.config');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
 
 var server = new WebpackDevServer(webpack(config), config.devServer)
   .listen(3000, 'localhost', function (err, result) {
