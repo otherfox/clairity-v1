e1ef3ce1f7fd2a53e306e75462c6bf9325663d72
Nick Tennies <nick@brainbytescreative.com>
2015-09-02 14:36:44 -0400

    makes menuItems accessible to tokenizer

--- src/ui/testbed/index.jsx
+++ src/ui/testbed/index.jsx
@@ -6,20 +6,28 @@
 import React, { PropTypes, Component } from 'react'
 import { contextTypes } from '../shared/decorators'
 import {
-  asyncTokenizer
+  asyncTokenizer,
+  asyncTypeahead
 } from '../shared/components/collectionDropdown'
 import async, { action, query, collection } from '../shared/components/async'
 import controllable from 'react-controllables'
 
 let NotifyTokenizer = asyncTokenizer({ collection: collection('contact').all() });
+let NotifyTypeahead = asyncTypeahead({ collection: collection('contact').all() });
 
-@controllable(['caller'])
+@controllable(['caller', 'caller2'])
 @contextTypes({muiTheme: PropTypes.object})
 class TestbedPage extends Component {
   render() {
     return (
-      <NotifyTokenizer value={ this.props.caller || '' }
-                       onOptionSelected={ i => this.props.onCallerChange(i) }/>
+      <div>
+        <h2>Tokenizer</h2>
+        <NotifyTokenizer value={ this.props.caller2 || '' }
+                         onOptionSelected={ i => this.props.onCaller2Change(i) }/>
+        <h2>Typeahead</h2>
+        <NotifyTypeahead value={ this.props.caller || '' }
+                         onOptionSelected={ i => this.props.onCallerChange(i) }/>
+      </div>
     )
   }
 }
