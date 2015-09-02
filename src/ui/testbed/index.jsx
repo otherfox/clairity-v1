4c220293bd0cdec5622ff90d929b17738b593dec
Nick Tennies <nick@brainbytescreative.com>
2015-09-02 14:11:33 -0400

    makes testbed controllable

--- src/ui/testbed/index.jsx
+++ src/ui/testbed/index.jsx
@@ -9,14 +9,17 @@ import {
   asyncTokenizer
 } from '../shared/components/collectionDropdown'
 import async, { action, query, collection } from '../shared/components/async'
+import controllable from 'react-controllables'
 
 let NotifyTokenizer = asyncTokenizer({ collection: collection('contact').all() });
 
+@controllable(['caller'])
 @contextTypes({muiTheme: PropTypes.object})
 class TestbedPage extends Component {
   render() {
     return (
-      <NotifyTokenizer />
+      <NotifyTokenizer value={ this.props.caller || '' }
+                       onOptionSelected={ i => this.props.onCallerChange(i) }/>
     )
   }
 }
