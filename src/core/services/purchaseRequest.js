ffdd8699caf5df55172cf3d6bd6b5a93f7c5fec3
Nick Tennies <nick@brainbytescreative.com>
2015-09-03 11:18:12 -0400

    changes purchaseRequest service to correct DAO

--- src/core/services/purchaseRequest.js
+++ src/core/services/purchaseRequest.js
@@ -5,7 +5,7 @@ import req from 'superagent'
 
 export function getPurchaseRequests() {
   return new Promise((s, f) => {
-    req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.TicketDAO&_m=getAllPurchaseRequests`)
+    req.get(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.PurchaseRequestDAO&_m=getAllPurchaseRequests`)
       .withCredentials()
       .end((err, res) => {
         if (!err) {
