0b03fa207ba33b5b602f4c627f21f1ac36f0acea
Nick Tennies <nick@brainbytescreative.com>
2015-09-03 12:16:21 -0400

    makes purchase requests page and gets data

--- src/ui/purchaseRequests/index.jsx
+++ src/ui/purchaseRequests/index.jsx
@@ -6,36 +6,30 @@ import Table from '../shared/components/table'
 import Header from '../shared/components/header'
 import { RaisedButton, Utils } from 'material-ui'
 import { contextTypes } from '../shared/decorators'
-import Selector from './selector'
+import async, { collection } from '../shared/components/async'
 
+@async({ purchaseRequests: collection('purchaseRequest').all() })
 @contextTypes({ muiTheme: PropTypes.object })
 class ViewPurchaseRequests extends Component {
 
-  getTickets(purchaseRequests) {
+  getPurchaseRequests(purchaseRequests) {
     console.log(purchaseRequests);
     return {
       colNames: [
-        { label: 'ID', name: 'id', props: { cellStyle: { color: Utils.ColorManipulator.fade(this.context.muiTheme.palette.primary1Color, .75) } } }
-        p.id,p.customer_id,
-        p.location_id,
-        p.amount,
-        p.item,
-        p.notes,
-        p.requested_by,
-        p.requested_date,
-        p.approval_id,
-        p.approval_date,
-        p.declined_id,
-        p.declined_date,
-        p.suggested_vendor_id,
-        p.received_id,
-        p.received_date,
-        pt.id as typeId,
-        pt.name as typeName,
-        ps.id as sourceId,ps.name as sourceName
+        { label: 'ID', name: 'id', props: { cellStyle: { color: Utils.ColorManipulator.fade(this.context.muiTheme.palette.primary1Color, .75) } } },
+        { label: 'item', name: 'item' },
+        { label: 'location_id', name: 'location_id' },
+        { label: 'approval_date', name: 'approval_date' },
+        { label: 'requested_by', name: 'requested_by' },
+        { label: 'approval_id', name: 'approval_id' },
+        { label: 'customer_id', name: 'customer_id' },
+        { label: 'requested_date', name: 'requested_date' },
+        { label: 'amount', name: 'amount' },
+        { label: 'type', name: 'type' },
+        { label: 'notes', name: 'notes' }
       ],
       data: purchaseRequests,
-      colWidths: [1,3,1,1,1,1,1],
+      // colWidths: [1,3,1,1,1,1,1],
       widthAdj: -20
     }
   }
@@ -45,29 +39,19 @@ class ViewPurchaseRequests extends Component {
       <Layout widths={{}} pPadding={'20px 20px 0 0'}>
         <Header><h1>Purchase Requests</h1></Header>
         <div>
-          <Link to='/tickets/create'>
-            <RaisedButton label='Create Ticket' />
+          <Link to='/purchaseRequests/create'>
+            <RaisedButton label='Create Purcase Request' />
           </Link>
         </div>
         <FilteredCollection data={this.props.purchaseRequests}>
           <Filters breakpoints={{md: 1200}}>
-            <TextFilter label='Subject' name='subject' />
-            <TextFilter label='Owner' name='owner' />
-            <RadioButtonFilter label={'Status'} name={'status'} buttonGroup={{name: 'status'}} options={[
-              { label: 'Open', value: 'Open'},
-              { label: 'New', value: 'New'},
-              { label: 'Both', value: '', defaultChecked: true}
-            ]} />
-            <DateFilter label='Received Min' name='received_date_time' future />
-            <DateFilter label='Received Max' name='received_date_time' past />
+            <TextFilter label='Item' name='item' />
           </Filters>
-          <Selector>
-            <Table {...this.getTickets(this.props.purchaseRequests)}/>
-          </Selector>
+          <Table {...this.getPurchaseRequests(this.props.purchaseRequests)}/>
         </FilteredCollection>
       </Layout>
     )
   }
 }
 
-export default networkCollectionRenderer(ViewTickets, { tableName: 'ticket' });
+export default ViewPurchaseRequests;
