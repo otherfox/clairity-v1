0a3ece47a47150c676f23182e938a1861e2b4eee
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-03 12:32:12 -0400

    adds all locations query to location list view

--- src/ui/locations/list.jsx
+++ src/ui/locations/list.jsx
@@ -1,10 +1,14 @@
 
-import React, { Component } from 'react'
+import { Component } from 'react'
+import async, { collection } from '../shared/components/async'
 
-class ListLocations extends Component {
+
+class ListLocationsView extends Component {
   render() {
     return <div />;
   }
 }
+
+export const AllLocations = async(ListLocations, { locations: collection('location').all() })
 
 export default ListLocations;
