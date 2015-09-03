57ce261f6acceba1c493014a7d9331b38cb6add4
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-03 13:02:18 -0400

    ListLocations uses location details component

--- src/ui/locations/list.jsx
+++ src/ui/locations/list.jsx
@@ -1,14 +1,31 @@
 
-import { Component } from 'react'
+import { Component, PropTypes } from 'react'
 import async, { collection } from '../shared/components/async'
+import { propTypes } from '../shared/decorators'
 
+import LocationDetails from './public/details'
 
+@propTypes({ locations: PropTypes.arrayOf(PropTypes.object) })
 class ListLocationsView extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        {
+          this.props.locations.map(l =>
+            <LocationDetails key={l.id} location={l} />
+          )
+        }
+      </div>
+    );
   }
 }
 
-export const AllLocations = async(ListLocations, { locations: collection('location').all() })
+export const AllLocations = async(ListLocations, {
+  locations: collection('location').all()
+});
+
+export const AccountLocations = async(ListLocations, {
+  locations: collection('location').by('account')
+});
 
 export default ListLocations;
