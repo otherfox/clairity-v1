4514f2a2e0fc49c545415070086c23dcd00475d6
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-04 01:26:43 -0400

    services/supportNotes#getSupportNotesByAccount uses utils

--- src/core/services/supportNote.js
+++ src/core/services/supportNote.js
@@ -1,10 +1,12 @@
+import { serialize } from './utils'
 
 import { withDelay } from 'memoize-promise'
-
 const memoize = withDelay(10000); // ten second delay
 
-const getSupportNotesByAccount = memoize(id =>
-  fetch(`https://lab.rairity.com/controller.cfm?event=serialize&authkey=tardis&_c=ample.dao.CustomerNoteDAO&_m=getAllCustomerNotesByCustomer&customer_id=${id}`)
+const CustomerNoteDAO = 'CustomerNoteDAO';
+
+const getSupportNotesByAccount = memoize(customer_id =>
+  fetch(...serialize(CustomerNoteDAO, 'getAllCustomerNotesByCustomer', { customer_id }))
     .then(res => res.json())
     .catch(console.error)
 );
