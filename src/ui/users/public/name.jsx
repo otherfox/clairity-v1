e8b7a640d7ea048e00f908e5a977042c4b1c0309
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-04 02:27:57 -0400

    UserName component actually uses lang now

--- src/ui/users/public/name.jsx
+++ src/ui/users/public/name.jsx
@@ -2,12 +2,12 @@ import React, { Component, PropTypes } from 'react'
 import { AgentCell } from '../../shared/components/table/tableCells'
 import { contextTypes } from '../../shared/decorators'
 
-@contextTypes
+@contextTypes({ lang: PropTypes.object })
 class UserName extends Component {
   render() {
     let agent = this.props.user ? this.props.user : {};
     return (
-      <AgentCell>{ agent.name || 'Not Assigned' }</AgentCell>
+      <AgentCell>{ agent.name || this.context.lang('Not Assigned') }</AgentCell>
     )
   }
 }
