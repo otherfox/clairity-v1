74404a21c0158923a5efc5891fea655e89a3e06c
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-04 03:06:14 -0400

    Utils is now extensible with fetch options

--- src/core/services/utils.js
+++ src/core/services/utils.js
@@ -2,6 +2,8 @@
 const urlBase = 'https://lab.rairity.com/controller.cfm?event='
 // serialize&authkey=tardis&_c=ample.dao.CustomerDAO&_m=getCustomerById&id=${id}'
 
-export function serialize(daoName, methodName, params) {
-  return [`${urlBase}serialize&authkey=tardis&_c=ample.dao.${daoName}&_m=${methodName}&` + Object.keys(params).map(k => `${k}=${params[k]}`).join('&'), { credentials: 'include' }];
+export function serialize(daoName, methodName, params, opts) {
+  return [`${urlBase}serialize&authkey=tardis&_c=ample.dao.${daoName}&_m=${methodName}&` +
+      Object.keys(params).map(k => `${k}=${params[k]}`).join('&'),
+      Object.assign({ credentials: 'include' }, opts)];
 }
