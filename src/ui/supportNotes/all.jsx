4c217e0127f8065e1a828557f5dc8c95437f5873
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-04 01:38:29 -0400

    Support note pages really fetch now

--- src/ui/supportNotes/all.jsx
+++ src/ui/supportNotes/all.jsx
@@ -2,6 +2,9 @@
 import React, { Component, PropTypes } from 'react'
 import { contextTypes, propTypes } from '../shared/decorators'
 
+import async, { collection } from '../shared/components/async'
+
+@async({ notes: collection('supportNote').all() })
 @contextTypes({ lang: PropTypes.object })
 @propTypes({ notes: PropTypes.arrayOf(PropTypes.object) })
 class ViewSupportNotes extends Component {
--- src/ui/supportNotes/index.jsx
+++ src/ui/supportNotes/index.jsx
@@ -2,6 +2,9 @@
 import React, { Component, PropTypes } from 'react'
 import { contextTypes, propTypes } from '../shared/decorators'
 
+import async, { collection } from '../shared/components/async'
+
+@async({ notes: collection('supportNote').by('account') })
 @contextTypes({ lang: PropTypes.object })
 @propTypes({ notes: PropTypes.arrayOf(PropTypes.object) })
 class ViewSupportNotes extends Component {
