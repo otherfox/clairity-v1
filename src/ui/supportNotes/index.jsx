8682c2c7aca7c705815f6916535c31ecc2b5f71d
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-04 01:10:28 -0400

    Adds propTypes/contextTypes to supportNotes page

--- src/ui/supportNotes/index.jsx
+++ src/ui/supportNotes/index.jsx
@@ -1,6 +1,9 @@
 
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { contextTypes, propTypes } from '../shared/decorators'
 
+@contextTypes({ lang: PropTypes.object })
+@propTypes({ notes: PropTypes.arrayOf(PropTypes.object) })
 class ViewSupportNotes extends Component {
   render() {
     return <div />;
