9bf47d6f23afd1b11bf94b58818aee4bd75c78e9
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-04 01:22:24 -0400

    Adds a service utils module

---
+++ src/core/services/utils.js
@@ -0,0 +1,7 @@
+
+const urlBase = 'https://lab.rairity.com/controller.cfm?event='
+// serialize&authkey=tardis&_c=ample.dao.CustomerDAO&_m=getCustomerById&id=${id}'
+
+export function serialize(daoName, methodName, params) {
+  return `${urlBase}serialize&authkey=tardis&_c=ample.dao.${daoName}&_m=${methodName}&` + Object.keys(params).map(k => `${k}=${params[k]}`).join('&');
+}
