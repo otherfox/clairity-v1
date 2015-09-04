d99e488851d80d50a8ae1998bbc1b74c1e8b401f
Ben Cochrane <ben.cochrane2112@gmail.com>
2015-09-04 02:54:53 -0400

    Utils correctly sends cookies

--- src/core/services/utils.js
+++ src/core/services/utils.js
@@ -3,5 +3,5 @@ const urlBase = 'https://lab.rairity.com/controller.cfm?event='
 // serialize&authkey=tardis&_c=ample.dao.CustomerDAO&_m=getCustomerById&id=${id}'
 
 export function serialize(daoName, methodName, params) {
-  return [`${urlBase}serialize&authkey=tardis&_c=ample.dao.${daoName}&_m=${methodName}&` + Object.keys(params).map(k => `${k}=${params[k]}`).join('&'), { credentials: 'same-origin' }];
+  return [`${urlBase}serialize&authkey=tardis&_c=ample.dao.${daoName}&_m=${methodName}&` + Object.keys(params).map(k => `${k}=${params[k]}`).join('&'), { credentials: 'include' }];
 }
